4a40a19ed5353ace480128f745a58f25c8c9b14e
firzencode <firzencode@live.cn>
2022-02-10 16:37:01 +0800

    Update osx-env-sync.sh
    feat: add .zshrc sync

--- osx-env-sync.sh
+++ osx-env-sync.sh
@@ -1,3 +1,7 @@
 grep "^export" $HOME/.bash_profile | while IFS=' =' read ignoreexport envvar ignorevalue; do
   launchctl setenv ${envvar} ${!envvar}
 done
+
+grep "^export" $HOME/.zshrc | while IFS=' =' read ignoreexport envvar ignorevalue; do
+  launchctl setenv ${envvar} ${!envvar}
+done
